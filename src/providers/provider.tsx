861b9b49532b51932b14f087fa078ee6577e82a1
Eric Tsai <eric492718@gmail.com>
2024-09-03 18:36:25 +0800

    change posthog config

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -19,7 +19,7 @@ export function PHProvider({ children, bootstrapData }: Props) {
   if (typeof window !== "undefined") {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      person_profiles: "always", // identified_only// or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
       capture_pageleave: true, // Enable pageleave capture
       bootstrap: bootstrapData,
